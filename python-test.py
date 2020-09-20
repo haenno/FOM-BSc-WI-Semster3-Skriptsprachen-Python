f13f0895b62a64e33d4b2ee80f8b42b983b60695
haenno <haenno@web.de>
2020-09-20 11:59:27 +0200

    more testing

--- python-test.py
+++ python-test.py
@@ -1,4 +1,13 @@
-print ("hello world")
+def prnt (parm):
+    print(str(parm) + "  ==>  " + eingText)
 
-x = input ("Ihr Name? ")
-print (x)
+while True:
+    i = input ("Wie oft gruessen? ")
+    eingText = "Hallo Welte, Erde,..."
+
+    while i > 0:
+        prnt(i)
+        i = i - 1
+        
+    print("Geschafft!")
+
