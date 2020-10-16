1fd2aebcaadbe273bc1890638692250ffdecb042
haenno <haenno@web.de>
2020-10-16 21:22:51 +0200

    Vorbereitung Vorlesung Sa.

--- zufallsWeb2.py
+++ zufallsWeb2.py
@@ -2,17 +2,42 @@
 
 from time import sleep
 import socket, ipaddress, threading, random
-from pprint import pprint
 
 max_threads = 50
 port = 80
 file_openports = "wwwips.txt"
+file_subnetsChecked = "wwwsubn.txt"
+
+def checkSubnetNew(subnetStr):
+    checked = False
+    return checked
+
+def writeResult(logEvent):
+    newIps = open(file_openports,'a')
+    newIps.write(logEvent+"\n")
+    newIps.close()
 
 def rndSubnet():
     min = 1
     max = 254
     seperator = '.'
-    return str(str(int(random.uniform(min,max)))+seperator+str(int(random.uniform(min,max)))+seperator+str(int(random.uniform(min,max)))+seperator+str("0"))
+    needNewSubnet = True
+    rtnStr = ""
+    while needNewSubnet:
+        tmpSubNet = str(str(int(random.uniform(min,max)))+seperator+str(int(random.uniform(min,max)))+seperator+str(int(random.uniform(min,max)))+seperator+str("0/24"))
+        known=0
+        doneSubNets = open(file_subnetsChecked,'r')
+        for subnet in doneSubNets:
+            if subnet == tmpSubNet:
+                known = known + 1
+        if known == 0:
+            needNewSubnet = False
+            rtnStr = tmpSubNet
+        doneSubNets.close()
+    doneSubNets = open(file_subnetsChecked,'a')
+    doneSubNets.write(rtnStr+"\n")
+    doneSubNets.close()
+    return rtnStr
 
 
 def check_port(ip, port):
@@ -22,26 +47,20 @@ def check_port(ip, port):
         socket.setdefaulttimeout(2.0) # seconds (float)
         result = sock.connect_ex((ip,port))
         if result == 0:
-            print ("http://"+str(ip)+":"+str(port)+" is open")
-            final[ip] = "http://"+str(ip)+":"+str(port)+" is open"
-            newIps.write("http://"+str(ip)+":"+str(port)+" is open")
-        else:
-            # print (str(ip)+":"+str(port)+"  is closed/filtered")
-            final[ip] = str(ip)+":"+str(port)+"  is closed/filtered"
+            writeResult("http://"+str(ip)+":"+str(port)+" is open")
         sock.close()
     except:
         pass
 
 while (True):
-	final = {}
-	newIps = open(file_openports,'a')
-
-	for ip in ipaddress.IPv4Network(rndSubnet()+'/24'): 
-	    threading.Thread(target=check_port, args=[str(ip), port]).start()
-	    #sleep(0.1)
-
-	# limit the number of threads.
-	while threading.active_count() > max_threads :
-	    sleep(1)
-
-	pprint(final)
+    scnip = ipaddress.IPv4Network(rndSubnet())
+    print ("Starte scan von " + str(scnip) + "...")
+    writeResult("Starte scan von " + str(scnip) + "...")
+    for ip in scnip: 
+        threading.Thread(target=check_port, args=[str(ip), port]).start()
+        sleep(0.01)
+    # limit the number of threads.
+    while threading.active_count() > max_threads :
+        sleep(1)
+    print ("...done. Next!")
+    writeResult("...done. Next!")
